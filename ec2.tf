9049a1f4ad4521bd4813c4290862dbd92a9162bd
YourVishu <vishumca51@gmail.com>
2024-08-23 01:12:52 +0530

    abc

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
 
-ami= "ami-03265a0778a880afb" #var.ami.id
+ami= "ami-0583d8c7a9c35822c" #var.ami.id
 instance_type="t2.micro"
 #vpc_security_group_ids = [aws_security_group-roboshop-all.id]
 
